228c6296b825abe1810d837a31db65f0dd2f6b17
Alvise461 <alvise.molino01@gmail.com>
2023-01-11 23:39:12 +0100

    da

--- Exception.cpp
+++ Exception.cpp
@@ -1,3 +1,6 @@
+//https://stackoverflow.com/questions/35068566/define-an-error-class-for-c
+//https://www.html.it/pag/64807/metodi-const/
+//https://cplusplus.com/doc/tutorial/exceptions/
 
 class InvalidInputException
         {
