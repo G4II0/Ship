d841c7a947a917cf32de78174543411df176f048
Alvise461 <alvise.molino01@gmail.com>
2023-01-11 19:48:40 +0100

    sdf

--- Exception.cpp
+++ Exception.cpp
@@ -1,8 +1,16 @@
 
 class InvalidInputException
         {
+                std::string InvalidInputException() const
+                {
+                        return "Input non valido";
+                }
         };
         // eccezione: mossa non valida
         class InvalidMoveException
         {
+                std::string InvalidMoveException() const
+                {
+                        return "Mossa non valida";
+                }
         };
