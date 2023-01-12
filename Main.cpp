454393e25fcce723471fe6aaaed2e19dc81e392d
G4II0 <smugallo@gmail.com>
2023-01-12 12:16:03 +0100

    asdfg

--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,7 @@ int main()
     string s = "s";
     char mar[12][12];
     string p1 = "p";
-    M::Mare board = new Mare();
+    M::Mare board = M::Mare();
     board.printAMare();
     return 0;
 }
