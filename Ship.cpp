ed286632ced2228b1a52709d37ca2ed0a0db2368
Neilkris01 <alvisestella@gmail.com>
2023-01-14 10:57:42 +0100

    tutto giusto tranne un po di errori

--- Ship.cpp
+++ Ship.cpp
@@ -13,7 +13,7 @@
         this -> dimensione = 0;
         this -> direzione = -1;
         this -> corazza = 0;
-        this -> pMedio = {0, 0};
+        //this -> pMedio = {0, 0};
         this -> numero = -1;
         this -> state = false;
     }
@@ -26,7 +26,7 @@
     {this -> pos = pos_;}
     void Ship::setPrua(std::pair<int, int> prua_)
     {this->prua = prua_;}
-    void Ship::setPrua(std::pair<int, int> poppa_)
+    void Ship::setPoppa(std::pair<int, int> poppa_)
     {this->poppa = poppa_;}
     void Ship::setPMedio(std::pair<int, int> pMedio_)
     {this -> pMedio = pMedio_;}
@@ -37,7 +37,7 @@
     void Ship::setCorazza(int corazza_)
     {this -> corazza = corazza_;}
     void Ship::setNumero(int numero_)
-    {this-> numero = numero;}
+    {this-> numero = numero_;}
 
     std::vector<char> Ship::getStructure() const
     {return this -> structure;}
