2e95b87ebe3608cddde62b95c3c446c915e2c3b8
G4II0 <smugallo@gmail.com>
2023-01-15 17:30:50 +0100

    vb

--- Players.hpp
+++ Players.hpp
@@ -12,6 +12,7 @@
         char Name;
         Mare mare;
         char Type;
+        int condition;
 
         public:
 
@@ -23,11 +24,13 @@
         void setName(char name);
         void setMare(Mare m);
         void setType(char type);
+        void setCondition(int condition);
         
         //get
         char getName() const;
         Mare getMare() const;
         char getType() const;
+        int getCondition() const;
 
         //Move
         void Move(std::pair<int, int> pos, std::pair<int, int> dest, Mare m);
