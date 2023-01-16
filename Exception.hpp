e68d84e75ed1c84975844b5e2d9188bcaf54cf89
Neilkris01 <alvisestella@gmail.com>
2023-01-16 12:48:34 +0100

    jndfwjnvfjn

--- Exception.hpp
+++ Exception.hpp
@@ -1,6 +1,7 @@
 #ifndef EXCEPTION_HPP
 #define EXCEPTION_HPP
 
+#include <exception>
 #include <string>
 
 // Generic class for exceptions
@@ -23,6 +24,17 @@ class Exception {
 		const std::string what(void) const {
 			return this->description_;
 		}
+};
+
+class invalidMoveException : public std::exception {
+    private:
+        std::string message_;
+    public:
+        invalidMoveException(const std::string &msg) : message_(msg) {}
+
+        const char* what(void) const {
+            return message_.c_str();
+        }
 };
 
 #endif
