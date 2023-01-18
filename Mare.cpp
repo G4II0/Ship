b4a145cde93f28156f46ba479b86eeab6c74cb38
G4II0 <smugallo@gmail.com>
2023-01-18 17:43:31 +0100

    wqd

--- Mare.cpp
+++ Mare.cpp
@@ -949,8 +949,9 @@ using namespace std;
                     cout << "C3" << endl;
                     cout << "Ytemp = " << y << " ";
                     cout << "C4" << endl;
-                    cout << x << y << " " << x << (y+1) << " " << x << (y-1) << " " << x << (y+2) << " " << x << (y-2) << endl;
-                    if (mar_[x][y+2] != ' ' || mar_[x][y+1] != ' ' || mar_[x][y] != ' ' || mar_[x][y-1] != ' ' || mar_[x][y-2] != ' ')
+                    vector<char> cv = mar_[x];
+                    cout << "C4.1" << endl;
+                    if (cv[y+2] != ' ' || mar_[x][y+1] != ' ' || mar_[x][y] != ' ' || mar_[x][y-1] != ' ' || mar_[x][y-2] != ' ')
                     {
                         cout << "C5" << endl;
                         for(int i = 0; i<v1.size()-1; i++)
