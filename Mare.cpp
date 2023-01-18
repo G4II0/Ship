b74b3adc12bf8936e3111b659b54a6e9c25b7202
G4II0 <smugallo@gmail.com>
2023-01-18 18:49:02 +0100

    w

--- Mare.cpp
+++ Mare.cpp
@@ -870,17 +870,17 @@ using namespace std;
             out += " ¦ ";
             for (int j = 0; j < mar_[i].size(); j++)
             {
-                //if (mar_[i][j] == ' ')
-                    //{
+                if (mar_[i][j] == ' ')
+                    {
                         out += " ";
                         //out += " │ ";
-                    //}
-                //else
-                //{
+                    }
+                else
+                {
                     //string s = to_string(mar_[i][j]);
                     //out += s;
-                //}
-                //out += mar_[i][j];
+                }
+                out += mar_[i][j];
                 out += " ¦ ";
             }
             out += "\n";
@@ -892,6 +892,7 @@ using namespace std;
         //out += "   └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘\n";
         out += "   +-----------------------------------------------+\n";
         out += "     A   B   C   D   E   F   G   H   I   J   K   L";
+        cout<<out<<endl;
         return out;
     }
 
