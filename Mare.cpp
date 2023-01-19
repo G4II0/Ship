6e965a3a47cfc8ebefc3c7a3195a53ee281925aa
G4II0 <smugallo@gmail.com>
2023-01-19 14:16:53 +0100

    sdfgh

--- Mare.cpp
+++ Mare.cpp
@@ -26,7 +26,7 @@ using namespace std;
         }
     }
 
-    void Mare::setMareAc(pair<int, int> p, vector<vector<char>> &mar_)
+    void Mare::setMareAc(pair<int, int> p, vector<vector<char> > &mar_)
     {
         int x = p.first;
         int y = p.second;
