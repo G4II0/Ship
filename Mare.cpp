a509635215709762c5cdb9a6635f1903e37f87ef
Neilkris01 <alvisestella@gmail.com>
2023-01-19 17:14:35 +0100

    ewrge

--- Mare.cpp
+++ Mare.cpp
@@ -917,7 +917,7 @@ using namespace std;
             else
             {
             out += "   +-----------------------------------------------+\n";
-            out += "     A   B   C   D   E   F   G   H   I   J   K   L\n";
+            out += "     1   2   3   4   5   6   7   8   9   10   11   12\n";
             break;
             }
 
