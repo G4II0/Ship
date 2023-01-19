73bdc057a90377ec7e0189e9a6b5ab56cbff87be
G4II0 <smugallo@gmail.com>
2023-01-19 21:40:14 +0100

    sd

--- Players.cpp
+++ Players.cpp
@@ -32,7 +32,7 @@
     std::string Players::getName() const
     {return this -> name_;}
     Mare &Players::getMare() const
-    {return this -> mare_;}
+    {return &mare_;}
     Mare Players::getMareN() const
     {return this -> mareNemico_;}
     char Players::getType() const
