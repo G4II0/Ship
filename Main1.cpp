1bcc0d3b570cd595ddbfc44240b8ca42ae6604da
G4II0 <smugallo@gmail.com>
2023-01-19 13:57:30 +0100

    csd

--- Main1.cpp
+++ Main1.cpp
@@ -74,7 +74,7 @@ int main()
     boardP1.printAMare();
     Mare boardP2;
     cout << "board2 vuota" << endl;
-    boardP2.printAMare();
+    boardP2.printAMare(); 
     players.push_back(Players(boardP1, names[0], types[0], boardP2));
     players.push_back(Players(boardP2, names[1], types[1], boardP1));
 
