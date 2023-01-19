195d45d3795160b86fb49b200b4193367621c8ce
Neilkris01 <alvisestella@gmail.com>
2023-01-19 21:18:44 +0100

    wd

--- Mare.cpp
+++ Mare.cpp
@@ -320,10 +320,9 @@ using namespace std;
             setMareAc(temp3);
             setMareAc(temp4);
             // posizionamento nel mareC
-        }//-
+        }
         else
         {
-            cout << "errore" << endl;
             throw InvalidInputException("Inserimento non valido");
         }
     }
